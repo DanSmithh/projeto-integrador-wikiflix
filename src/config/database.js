9ff907ea835e20807ba3827bbd29928a4b70a216
DanSmithh <daniel.odsilvas@gmail.com>
2022-07-09 17:20:08 -0300

    configurando heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
   username: 'b2063afff92666',
   password: '5ce90875',
-  database: 'db_wikiflix',
+  database: 'heroku_8b5e14d07fdef03',
   host: 'us-cdbr-east-06.cleardb.net',
   dialect: 'mysql'
 }
